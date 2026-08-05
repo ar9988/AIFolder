88fe3f4500300c62631e90091644751312f73911
master <kar870428@gmail.com>
2026-08-06 00:58:55 +0900

    chore: version code update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.ar9988.tagfilemanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.6"
+        versionCode = 18
+        versionName = "1.1.7"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
